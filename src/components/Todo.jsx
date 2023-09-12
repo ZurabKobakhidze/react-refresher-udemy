c83bdb76d0f98b48df05b0224d36929224fc1fc8
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-12 15:32:51 +0400

    32 chapter

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,24 @@
-function Todo(props) {
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
-    function deleteHandler() {
-        
-    }
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
